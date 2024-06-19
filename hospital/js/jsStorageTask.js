85dde16ab1c26166d4292b8b8579154a28c0cca2
reemnedal <eng.reemnedal@gmail.com>
2024-06-19 21:02:16 +0300

    regex task 19/6/2024

--- hospital/js/jsStorageTask.js
+++ hospital/js/jsStorageTask.js
@@ -1,5 +1,7 @@
         //this solution objects inside array we have method two in the end of code 
- 
+        // localStorage.clear();
+        
+
         let allData =  [];
             function render() {
 
@@ -11,6 +13,83 @@
                 let date     = form.date.value;
                 let gender   = form.gender.value;
                 let diseases = form.diseases.value;
+                let email =form.email.value;
+
+
+
+                if (name.includes(' ')) {
+                    let divNote = document.querySelector(".notification");
+                   
+                    divNote.innerHTML = "<span style='color: red;'>Username is invalid. It should not contain spaces. </span><br>";
+                    return; // Exit function early if validation fails
+                }else{
+
+                 
+                    // Check length (more than 8 characters)
+                    if (password.length < 8) {
+                        let divNote = document.querySelector(".notification");
+                   
+                        divNote.innerHTML = " <span style='color: red;'>password must be  more than 8 characters<br> Password must have at least one uppercase letter.<br> password must have at least one digit<br>password must have at least one special character </span>";
+
+
+                       
+                        return false;
+                    }else{               
+                    // Check for at least one uppercase letter
+                    if (!/[A-Z]/.test(password)) {
+                        let divNote = document.querySelector(".notification");
+                   
+                        divNote.innerHTML = "<span style='color:green;'>password must be  more than 8 characters<br></span><span style='color: red;'>Password must have at least one uppercase letter.<br> password must have at least one digit<br>password must have at least one special character </span>";
+
+                        return false;
+                    }else{
+                
+                    // Check for at least one digit
+                    if (!/\d/.test(password)) {
+                        let divNote = document.querySelector(".notification");
+                   
+                        divNote.innerHTML = "<span style='color:green;'>password must be  more than 8 characters<br>Password must have at least one uppercase letter.<br></span><span style='color: red;'> password must have at least one digit<br>password must have at least one special character </span>";
+
+
+                        return false;
+                    }else{
+                
+                    // Check for at least one special character
+                    if (!/[!@#$%^&*]/.test(password)) {
+                        let divNote = document.querySelector(".notification");
+                   
+                        divNote.innerHTML = "<span style='color:green;'>password must be  more than 8 characters<br>Password must have at least one uppercase letter.<br> password must have at least one digit<br></span><span style='color: red;'>password must have at least one special character </span>";
+
+
+                        return false;
+                    }else{
+                
+                
+                //     // Check for at least one numeric digit
+                //     if (!/\d/.test(password)) {
+                //         return false;
+                //     }
+                
+                //     // Check for at least one uppercase letter
+                //     if (!/[A-Z]/.test(password)) {
+                //         return false;
+                //     }
+                
+                //     // Check for at least one special character (defined as non-alphanumeric)
+                //     if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
+                //         return false;
+                //     }
+                
+                //     // All conditions passed
+                //     return true;
+                // }
+                
+                // // Example usage:
+                //  if (validatePassword(password)) {
+                //     console.log("Password is valid!");
+                // } else {
+                //     console.log("Password does not meet requirements.");
+                // }
 
                 // Create an object to store the new data
                 let newData = {
@@ -26,7 +105,37 @@
                  allData = JSON.parse(localStorage.getItem("allData")) || [];
 
 
-               
+                let nameExist=  allData.some(item => item.name == name);
+
+
+
+                 if(nameExist){
+
+                    let divNote=document.querySelector(".notification");
+                    divNote.innerHTML = "<span style='color: red;'>  Name already exist !! </span><br>";
+                    ;
+                                 }
+
+
+                                 else{
+
+                                    if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
+                                      
+                    let divNote=document.querySelector(".notification");
+                    divNote.innerHTML = "<span style='color: red;'> Invalid date format   </span><br>";
+
+                                    } else {
+
+                                        if (!/^07\d{8}$/.test(phone)) {
+                                             let divNote=document.querySelector(".notification");
+                                            divNote.innerHTML = "<span style='color: red;'>invalid phone number </span><br>";
+                        
+
+                                        } else {
+                                         
+                                    
+                 
+                                
                 // let newData=[name,password,phone,date,gender,diseases]; 
                allData.push(newData);
                
@@ -45,7 +154,7 @@
                 console.log(printValue[0].gender);
                 console.log(printValue[0].date);
 
-  let section = document.querySelector(".section"); let mainUl = document.createElement("ul");
+                let section = document.querySelector(".section"); let mainUl = document.createElement("ul");
               printValue.forEach(item => {
                
                    
@@ -101,7 +210,8 @@
 
             
                     // mainDiv.appendChild(text);
-                } );
+                } );}  }}}}}}} 
+
 
 
 
@@ -143,6 +253,12 @@
          
         // }
             }
+
+
+
+    
+            
+            
 
             // DOMContentLoaded.addEventListener("load", cardData);
 
